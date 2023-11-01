e38c3119a2bf95703f373df8849dd20cd416ff0e
Marena-web <marenapereyra@gmail.com>
2023-11-01 20:25:01 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,3 +11,16 @@ for (let año = añoInicio; año <= añoFinal; año++) {
 }
 
 años.insertAdjacentHTML("beforeend", opcionesHTML);
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (marcas) {
+    for (const marca of marcas) {
+      marcasContainer.insertAdjacentHTML("beforeend");
+    }
+  });
+
+
+  // fetch suelto, uno es el que tienen los autos y otro que trae unos array con strings que  cada string son las diferentes marcas,hecho ese fetch hacemos un .then incertadjacenthtml) //
