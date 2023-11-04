f8eae6c38d7ae295a6fd1b2fd341aff972678294
Marena-web <marenapereyra@gmail.com>
2023-11-03 21:06:24 -0300

    Realice la interacción de marcas,modelo y empece a hacer el extra1

--- js/app.js
+++ js/app.js
@@ -13,13 +13,46 @@ for (let año = añoInicio; año <= añoFinal; año++) {
 años.insertAdjacentHTML("beforeend", opcionesHTML);
 
 
-fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
- .then(function (datosDelServidor) {
- return datosDelServidor.json();
- })
- .then(function (personaje) {
- return console.log("Nombre del personaje: " + personaje.name);
- });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const selectElement = document.getElementById("marcas");
 
-  // fetch suelto, uno es el que tienen los autos y otro que trae unos array con strings que  cada string son las diferentes marcas,hecho ese fetch hacemos un .then incertadjacenthtml) //
+  fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
+    .then((response) => response.json())
+    .then((data) => {
+      let optionsHTML = "";
+
+      for (let i = 0; i < data.length; i++) {
+        optionsHTML += `<option value="${data[i]}">${data[i]}</option>`;
+      }
+
+      selectElement.insertAdjacentHTML("beforeend", optionsHTML);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectElement = document.getElementById("modelos");
+
+  fetch("https://ha-front-api-proyecto-final.vercel.app/models?brand=Audi")
+    .then((response) => response.json())
+    .then((data) => {
+      let optionsHTML = "";
+
+      for (let i = 0; i < data.length; i++) {
+        optionsHTML += `<option value="${data[i]}">${data[i]}</option>`;
+      }
+
+      selectElement.insertAdjacentHTML("beforeend", optionsHTML);
+    });
+});
+
+// Agarrar de las options el valor de año, marca (brand), y el modelo. y guardarlo en constantes
+
+const year = ;
+const brand = ;
+const model = ;
+
+fetch(`https://ha-front-api-proyecto-final.vercel.app/cars?year=${year}&brand=${brand}&m
+odel=${model}
+`)
+
